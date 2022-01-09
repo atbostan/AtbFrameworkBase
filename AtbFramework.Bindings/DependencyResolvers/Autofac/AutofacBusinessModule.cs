228b75201ea7ec146fe42b726af46deebe018e6c
Ahmet Tarık Bostan <a.tarikbostan@gmail.com>
2022-01-09 22:48:57 +0300

    aspect interceptor selectot and autofacbusiness module added

---
+++ AtbFramework.Bindings/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,33 @@
+﻿using AtbFramework.Application.Business;
+using AtbFramework.Application.Interfaces.Business;
+using AtbFramework.Application.Interfaces.Repository;
+using AtbFramework.Infrastructure.Interceptors;
+using AtbFramework.Persistance.Repositories;
+using Autofac;
+using Autofac.Extras.DynamicProxy;
+using Castle.DynamicProxy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtbFramework.Bindings.DependencyResolvers.Autofac
+{
+    /// <summary>
+    /// This class use for DI with the help of Autofac
+    /// </summary>
+    public class AutofacBusinessModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+
+            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+            builder.RegisterAssemblyTypes(assembly).AsImplementedInterfaces()
+                .EnableInterfaceInterceptors(new ProxyGenerationOptions()
+                {
+                    Selector = new AspectInterceptorSelector()
+                }).SingleInstance();
+        }
+    }
+}
---
+++ AtbFramework.Infrastructure/Interceptors/AspectInterceptorSelector.cs
@@ -0,0 +1,28 @@
+﻿using Castle.DynamicProxy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtbFramework.Infrastructure.Interceptors
+{
+    public class AspectInterceptorSelector : IInterceptorSelector
+    {
+        public IInterceptor[] SelectInterceptors(Type type, MethodInfo method, IInterceptor[] interceptors)
+        {
+            // Get the attributes of class , if any
+            var classAttributes = type.GetCustomAttributes<MethodInterceptorBaseAttribute>(true).ToList();
+
+            //Get the attributes of method, if any
+            var methodAttributes = type.GetMethod(method.Name).GetCustomAttributes<MethodInterceptorBaseAttribute>(true).ToList();
+
+            
+            classAttributes.AddRange(methodAttributes);
+
+            return classAttributes.OrderBy(x => x.Priority).ToArray();
+            
+        }
+    }
+}
